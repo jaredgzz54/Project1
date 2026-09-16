dc5974dc52202e6c4b08e4765f15e5daeb818da5
OMelendez227 <melendezoscar227@gmail.com>
2026-09-16 14:39:01 -0500

    Update Resource.h

--- Resource.h
+++ Resource.h
@@ -1,5 +1,26 @@
 #ifndef RESOURCE_H
 #define RESOURCE_H
 
+#include <string>
+using namespace std;
+
+class Resource{
+private:
+  int resourceld;
+  string name;
+  bool isAvailable;
+
+public:
+  Resource();
+  Resource(int id, string resName, bool available);
+
+int getResource() const;
+string getName() const;
+bool getAvailability() const;
+
+void setAvailability(bool available);
+void display() const;
+
+};
 
 #endif RESOURCE_H
