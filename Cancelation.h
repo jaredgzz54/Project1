496eabcd2e21214686da9cd8c85e589d021bee3f
Isaac N. <boomcreeper2000@aol.com>
2026-09-20 10:36:45 -0500

    Update Cancelation.h
    Added barebone code structure

--- Cancelation.h
+++ Cancelation.h
@@ -1,6 +1,27 @@
 #ifndef CANCELATION_H
 #define CANCELATION_H
 
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+struct CancelationNode {
+
+};
+
+/*
+	Stack Functions (Context is CancelationHistory)
+		CancelationHistory.top();	// Access first element (Can be used as a read or write)
+		CancelationHistory.push();	// Add element to top of stack (Requires data)
+		CancelationHistory.pop();	// Removes top element of stack
+		CancelationHistory.empty(); // Checks if stack is empty (If empty will output 0
+*/
+stack<CancelationNode> CancelationHistory;
+
+
+
+
 
 
 #endif
