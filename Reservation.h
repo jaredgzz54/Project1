f4a0b98f830788c9f61b793e32ca4401c18a46e7
Isaac N. <boomcreeper2000@aol.com>
2026-09-20 23:09:30 -0500

    Update Reservation.h

--- Reservation.h
+++ Reservation.h
@@ -37,4 +37,28 @@ public:
     Node* DLLSearch(DoublyLinkedList* list, int itemToSearch);
 };
 
+
+// Cancelation code \\
+/* 
+    Structure for a cancelation node
+*/
+struct CancelationNode {
+	string studentName;
+
+};
+
+/*
+	Stack Functions (Context is CancelationHistory)
+		CancelationHistory.top();	// Access first element (Can be used as a read or write)
+		CancelationHistory.push();	// Add element to top of stack (Requires data)
+		CancelationHistory.pop();	// Removes top element of stack
+		CancelationHistory.empty(); // Checks if stack is empty (If empty will output 0
+*/
+class CancelationHistoryClass {
+	private:
+		stack<CancelationNode> CancelationHistory;
+
+	public:
+		void DisplayCancelationHistory(); // Cannot be declared as const because what we plan to do with stack
+};
 #endif
