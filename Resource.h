bbb69f3cf15ba1cd668ba38e4e9a29e65c6787f9
OMelendez227 <melendezoscar227@gmail.com>
2026-09-20 22:27:07 -0500

    Update Resource.h

--- Resource.h
+++ Resource.h
@@ -1,26 +1,53 @@
 #ifndef RESOURCE_H
 #define RESOURCE_H
 
+#include <iostream>
+#include <queue>
 #include <string>
-using namespace std;
 
-class Resource{
-private:
-  int resourceld;
-  string name;
-  bool isAvailable;
-
-public:
-  Resource();
-  Resource(int id, string resName, bool available);
-
-int getResource() const;
-string getName() const;
-bool getAvailability() const;
-
-void setAvailability(bool available);
-void display() const;
-
+struct WaitingRequest {
+    std::string studentId;
+    std::string studentName;
+    std::string resourceId;
 };
 
-#endif RESOURCE_H
+class WaitingList {
+private:
+    std::string resourceId;
+    std::queue<WaitingRequest> requests;
+
+public:
+    explicit WaitingList(const std::string& resourceID = "");
+
+    bool enqueue(const WaitingRequest& request);
+    bool dequeue(WaitingRequest& next);
+    bool removeStudent(const std::string& studentID);
+    bool peek(WaitingRequest& front) const;
+    bool contains(const std::string& studentID) const;
+    bool isEmpty() const;
+    int size() const;
+    std::string getResourceId() const;
+    void display() const;
+};
+
+class Resource {
+private:
+    std::string resourceId;
+    std::string resourceName;
+    std::string resourceType;
+    bool isAvailable;
+
+public:
+    Resource();
+    Resource(std::string id, std::string name, std::string type, bool available);
+
+    std::string getResourceId() const;
+    std::string getResourceName() const;
+    std::string getResourceType() const;
+    bool getAvailability() const;
+
+    void setAvailability(bool available);
+    void display() const;
+};
+
+#endif
