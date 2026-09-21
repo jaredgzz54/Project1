0571ebcf680b45fe5b45030550fc83f5b53ea565
Isaac N. <boomcreeper2000@aol.com>
2026-09-20 23:11:29 -0500

    Update Reservation.cpp
    Merging Cancelation.cpp to Reservation.cpp

--- Reservation.cpp
+++ Reservation.cpp
@@ -99,3 +99,34 @@ void DoublyLinkedList::DLLRemove(DoublyLinkedList* list, Node* itemToRemove)
 
     delete itemToRemove;
 }
+
+
+
+// Cancelation cpp Code
+void CancelationHistoryClass::DisplayCancelationHistory() {
+	int const arrSize = CancelationHistory.size();
+	vector<CancelationNode> data;
+
+	// Ittrates throughout entire stack and displays information
+	while (CancelationHistory.empty() == 1) {
+
+		// Display information here
+		cout << CancelationHistory.top().studentName << endl;
+
+		// Add item to vector then delete top
+		data.push_back(CancelationHistory.top());
+		CancelationHistory.pop();
+	}
+
+
+
+	// Add items back to stack
+	while (data.empty() == 1) {
+
+		// Adds item 0 from vector to stack again
+		CancelationHistory.push(data.at(0));
+
+		// Delete item 0 from vector
+		data.erase(data.begin());
+	}
+}
